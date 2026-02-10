df65a7238b733dad9f1296e148c1b64a006b7957
johnny <johnny.chow.19@gmail.com>
2026-02-11 07:41:08 +1000

    Direction

--- simulator/src/Simulator/Robots/Direction.cs
+++ simulator/src/Simulator/Robots/Direction.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Simulator.Robots;
 
 public sealed class Direction : IEquatable<Direction>
@@ -7,6 +9,7 @@ public sealed class Direction : IEquatable<Direction>
     public static readonly Direction South = new("SOUTH", 0, -1);
     public static readonly Direction West = new("WEST", -1, 0);
 
+    // Directions ordered clockwise
     private static readonly Direction[] Directions = [North, East, South, West];
 
     private readonly string _name;
@@ -20,19 +23,13 @@ public sealed class Direction : IEquatable<Direction>
         _deltaY = deltaY;
     }
 
-    public Direction RotateLeft() => this == North ? West :
-                                      this == West ? South :
-                                      this == South ? East :
-                                      North;
+    public Direction RotateLeft() => Directions[(Array.IndexOf(Directions, this) + 3) % 4];
 
-    public Direction RotateRight() => this == North ? East :
-                                       this == East ? South :
-                                       this == South ? West :
-                                       North;
+    public Direction RotateRight() => Directions[(Array.IndexOf(Directions, this) + 1) % 4];
 
     public (int DeltaX, int DeltaY) GetMovementDelta() => (_deltaX, _deltaY);
 
-    public static bool TryParse(string value, out Direction? direction)
+    public static bool TryParse(string value, [NotNullWhen(true)] out Direction? direction)
     {
         direction = Directions.FirstOrDefault(d => 
             string.Equals(d._name, value, StringComparison.OrdinalIgnoreCase));
